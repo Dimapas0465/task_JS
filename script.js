2720c47030703d2257f74c26adc3ed868356b75a
Pas_Dmitriy <anban798@gmail.com>
2024-10-22 15:00:59 +0300

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 // Виведення повідомлення у консоль
 console.log("Це повідомлення з зовнішнього JavaScript файлу!");
 alert("Доброго дня, мене звати Пасічник Дмитро")
+// Функція для обчислення  суми та різниці 
+function calculateSumAndDifference(a, b) {
+    let sum = a + b;
+    let difference = a - b;
+    //Виведення резульатів у консоль 
+    console.log("Сума: " + sum);
+    console.log("Різниця: " + difference)
+}
+
+//Виклик функції з прикладними числами
+calculateSumAndDifference(24, 7);
